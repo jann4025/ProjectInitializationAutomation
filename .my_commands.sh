75d132fc6c16e915fa4d59eff821b3c8bab680b3
Jannick Holm <43129680+jann4025@users.noreply.github.com>
2019-07-23 14:54:53 +0200

    Update .my_commands.sh

--- .my_commands.sh
+++ .my_commands.sh
@@ -3,12 +3,12 @@
 function create() {
     cd
     python create.py $1
-    cd /Users/kalle/Documents/Projects/MyProjects/$1
+    cd /Users/jannickholm/dropbox/coding/$1
     git init
-    git remote add origin git@github.com:KalleHallden/$1.git
+    git remote add origin git@github.com:jann4025/$1.git
     touch README.md
     git add .
     git commit -m "Initial commit"
     git push -u origin master
-    code .
+    brackets .
 }
